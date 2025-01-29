3b36015419b6615ee566c197af58eddc088423b8
Natanael Araújo <natanael@hotmail.co.jp>
2025-01-28 23:58:29 -0300

    feat(payment): update success page links and content

--- src/app/success-payment/page.tsx
+++ src/app/success-payment/page.tsx
@@ -17,7 +17,7 @@ export default function SuccessPayment() {
           Agora falta você desbloquear seu acesso ao ReviewPix, então não atualize, não clique em “voltar” e muito menos feche essa página!
         </p>
         <h3 className='mt-4 px-5 font-bold text-[19px] text-center text-primary md:text-2xl uppercase'>ATENÇÃO, VEJA ESSE VÍDEO ATÉ O FINAL PARA ACESSAR O FEEDFY!</h3>
-        <Player src="/vsl-2.mp4" link='/pay' />
+        <Player src="/vsl-2.mp4" link='https://checkout.perfectpay.com.br/pay/PPU38CPD6SH?upsell=true&utm_source=13b1b66d-a1d6-481d-a583-4094f2f21a53%3A%3A%3A%3A&subid=13b1b66d-a1d6-481d-a583-4094f2f21a53&xcod=13b1b66d-a1d6-481d-a583-4094f2f21a53%3A%3A%3A%3AhQwK21wXxRhQwK21wXxRhQwK21wXxRhQwK21wXxR&sck=13b1b66d-a1d6-481d-a583-4094f2f21a53%3A%3A%3A%3AhQwK21wXxRhQwK21wXxRhQwK21wXxRhQwK21wXxR' />
       </main>
     </div>
   );
