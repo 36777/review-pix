6452b37cf2dce7c3f19f71f1f98a38bd91a9a19d
Natanael Araújo <natanael@hotmail.co.jp>
2025-01-28 23:27:15 -0300

    ✨ feat: update success page and redirect to upsell

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -15,10 +15,10 @@ export default function Success() {
         <p className='px-4 text-white md:text-2xl italic'>
           Obs: O valor pode demorar até 1 minuto para cair.
         </p>
-        <p className='px-4 text-white md:text-2xl'>
+        {/* <p className='px-4 text-white md:text-2xl'>
           Agora basta <b>dar sua opinião</b> em mais estabelecimentos para realizar seu <b>primeiro saque</b>
-        </p>
-        <Link href={'/avaliacao'}>
+        </p> */}
+        <Link href={'/upsell'}>
           <button className='bg-white px-8 py-2 rounded-full font-bold text-2xl text-primary transition-all animate-pulse hover:scale-105'>CONTINUAR</button>
         </Link>
       </div>
